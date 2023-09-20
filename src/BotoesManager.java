9be011899fb9332213abfb3c59802e6cad8aec09
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-09-20 13:32:49 -0300

    fix: adição do painel à classe BOtoesManager

--- src/BotoesManager.java
+++ src/BotoesManager.java
@@ -1,21 +1,35 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 
-public abstract class BotoesManager {
-    private JPanel panel; // Adicione um campo para armazenar o painel;
+public class BotoesManager {
+    private JPanel panel;
     private JFrame frame;
 
     public BotoesManager(JFrame frame) {
         this.frame = frame;
-        panel = new JPanel(); // Inicialize o painel no construtor
-    }
-    //Método para criar e adicionar um botão à GUI
-    public void adicionarBotao(String nomeBotao) {
-
+        this.panel = new JPanel(new GridLayout(4, 2)); // GridLayout com 2 linhas e 1 coluna
     }
 
     public JPanel getPanel() {
         return panel;
     }
 
+    public void adicionarBotao(String nomeBotao, Class<?> classeEspecifica) {
+        ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    GraficoBase grafico = (GraficoBase) classeEspecifica.getDeclaredConstructor().newInstance();
+                    grafico.exibir();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+        JButton botao = new JButton(nomeBotao);
+        botao.addActionListener(actionListener);
+        
+        panel.add(botao); // Adicione o botão diretamente ao painel
+    }
 }
