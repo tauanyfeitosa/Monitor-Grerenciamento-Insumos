a729c8220b9741131ec0213919d04537746ce94b
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-09-26 22:25:32 -0300

    fix: adição do parâmetro anoSelecionado ao construtor, instanciamento do parâmetro que referencia ManipulacaoCSV para retornar o dataset, remoção dos métodos para leitura do csv que foram transferidos para ManipulacaoCSV

--- src/GraficoBase.java
+++ src/GraficoBase.java
@@ -1,13 +1,8 @@
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import java.io.IOException;
-import java.nio.file.Paths;
 import javax.swing.*;
 import java.awt.*;
 
@@ -16,63 +11,40 @@ abstract class GraficoBase {
     private final String saidaColumn;
     private final String titulo;
     private final String eixoXLabel;
+    private final String anoSelecionado; // Adicione o ano selecionado
+    private final ManipulacaoCSV manipulacaoCSV;
 
-    protected GraficoBase(String entradaColumn, String saidaColumn, String titulo, String eixoXLabel) {
+    public GraficoBase(String entradaColumn, String saidaColumn, String titulo, String anoSelecionado) {
         this.entradaColumn = entradaColumn;
         this.saidaColumn = saidaColumn;
         this.titulo = titulo;
-        this.eixoXLabel = eixoXLabel;
+        this.eixoXLabel = "Meses referente ao ano de " + anoSelecionado;
+        this.anoSelecionado = anoSelecionado; // Inicialize o ano selecionado
+        this.manipulacaoCSV = new ManipulacaoCSV(anoSelecionado, entradaColumn, saidaColumn);
     }
 
     protected DefaultCategoryDataset createDataset() {
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-        try {
-            CSVParser parser = CSVParser.parse(
-                Paths.get("dados.csv").toFile(),
-                java.nio.charset.StandardCharsets.UTF_8,
-                CSVFormat.DEFAULT.withFirstRecordAsHeader()
-            );
-
-            int saldo = 0;
-
-            for (CSVRecord record : parser) {
-                String mes = record.get("Mes");
-
-                int entrada = Integer.parseInt(record.get(entradaColumn));
-                int saida = Integer.parseInt(record.get(saidaColumn));
-
-                saldo += entrada - saida;
-
-                dataset.addValue(entrada, "Entrada", mes);
-                dataset.addValue(saida, "Saída", mes);
-                dataset.addValue(saldo, "Saldo", mes);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return dataset;
+        return manipulacaoCSV.createDataset(); // Use o dataset retornado pela classe ManipulacaoCSV
     }
 
     protected JFreeChart createChart(DefaultCategoryDataset dataset) {
         JFreeChart chart = ChartFactory.createBarChart(
-            titulo,
-            eixoXLabel,
-            "Quantidade de Insumos",
-            dataset,
-            PlotOrientation.VERTICAL,
-            true,
-            true,
-            false
+                titulo,
+                eixoXLabel,
+                "Quantidade de Insumos",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
         );
 
         return chart;
     }
 
-    protected abstract String getTitulo();
+    protected abstract String getTitulo(); // Método abstrato
 
-    protected void exibir() {
+    public void exibir() {
         JFrame frame = new JFrame(getTitulo());
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -85,5 +57,3 @@ abstract class GraficoBase {
         frame.setVisible(true);
     }
 }
-
-
