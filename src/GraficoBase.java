624ab291ec4ffb8f87021566a4b7c22dbd171288
Maria Tauany Santos Feitosa <tauanysanttos13@gmail.com>
2023-09-26 22:41:29 -0300

    add: documentação do código para melhor entendimento

--- src/GraficoBase.java
+++ src/GraficoBase.java
@@ -1,11 +1,14 @@
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-import javax.swing.*;
-import java.awt.*;
+import org.jfree.chart.JFreeChart; // Importa a classe JFreeChart do pacote org.jfree.chart para criar gráficos.
+import org.jfree.chart.ChartFactory; // Importa a classe ChartFactory do pacote org.jfree.chart para criar gráficos.
+import org.jfree.chart.ChartPanel; // Importa a classe ChartPanel do pacote org.jfree.chart para exibir gráficos em uma interface gráfica.
+import org.jfree.chart.plot.PlotOrientation; // Importa a classe PlotOrientation do pacote org.jfree.chart.plot para definir a orientação do gráfico.
+import org.jfree.data.category.DefaultCategoryDataset; // Importa a classe DefaultCategoryDataset do pacote org.jfree.data.category para criar o conjunto de dados.
+import javax.swing.*; // Importa o pacote javax.swing para criar interfaces gráficas.
+import java.awt.*; // Importa o pacote java.awt para trabalhar com componentes gráficos.
 
+/**
+ * Esta classe abstrata, GraficoBase, serve como base para classes que criam gráficos usando JFreeChart.
+ */
 abstract class GraficoBase {
     private final String entradaColumn;
     private final String saidaColumn;
@@ -14,6 +17,13 @@ abstract class GraficoBase {
     private final String anoSelecionado; // Adicione o ano selecionado
     private final ManipulacaoCSV manipulacaoCSV;
 
+    /**
+     * Construtor da classe GraficoBase.
+     * @param entradaColumn A coluna que contém os valores de entrada.
+     * @param saidaColumn A coluna que contém os valores de saída.
+     * @param titulo O título do gráfico.
+     * @param anoSelecionado O ano selecionado para filtrar os dados.
+     */
     public GraficoBase(String entradaColumn, String saidaColumn, String titulo, String anoSelecionado) {
         this.entradaColumn = entradaColumn;
         this.saidaColumn = saidaColumn;
@@ -23,10 +33,19 @@ abstract class GraficoBase {
         this.manipulacaoCSV = new ManipulacaoCSV(anoSelecionado, entradaColumn, saidaColumn);
     }
 
+    /**
+     * Este método cria um conjunto de dados para o gráfico.
+     * @return Um DefaultCategoryDataset contendo os dados processados.
+     */
     protected DefaultCategoryDataset createDataset() {
         return manipulacaoCSV.createDataset(); // Use o dataset retornado pela classe ManipulacaoCSV
     }
 
+    /**
+     * Este método cria o gráfico usando o conjunto de dados fornecido.
+     * @param dataset O conjunto de dados para o gráfico.
+     * @return Um objeto JFreeChart configurado com os dados.
+     */
     protected JFreeChart createChart(DefaultCategoryDataset dataset) {
         JFreeChart chart = ChartFactory.createBarChart(
                 titulo,
@@ -42,8 +61,15 @@ abstract class GraficoBase {
         return chart;
     }
 
+    /**
+     * Este método abstrato deve ser implementado pelas classes filhas para obter o título do gráfico.
+     * @return O título do gráfico.
+     */
     protected abstract String getTitulo(); // Método abstrato
 
+    /**
+     * Este método exibe o gráfico em uma janela Swing.
+     */
     public void exibir() {
         JFrame frame = new JFrame(getTitulo());
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
--- src/ManipulacaoCSV.java
+++ src/ManipulacaoCSV.java
@@ -1,31 +1,47 @@
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.TreeMap;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.jfree.data.category.DefaultCategoryDataset;
+import java.io.IOException; // Importa a classe IOException para tratamento de exceções de E/S (entrada/saída).
+import java.nio.file.Paths; // Importa a classe Paths do pacote java.nio.file para manipulação de caminhos de arquivo.
+import java.util.ArrayList; // Importa a classe ArrayList do pacote java.util para criar uma lista de registros.
+import java.util.Collections; // Importa a classe Collections do pacote java.util para ordenação de registros.
+import java.util.Comparator; // Importa a classe Comparator do pacote java.util para definir uma regra de comparação personalizada.
+import java.util.List; // Importa a classe List do pacote java.util para trabalhar com listas.
+import java.util.TreeMap; // Importa a classe TreeMap do pacote java.util para armazenar saldos por ano de forma ordenada.
+import org.apache.commons.csv.CSVFormat; // Importa a classe CSVFormat do pacote org.apache.commons.csv para definir o formato de leitura do arquivo CSV.
+import org.apache.commons.csv.CSVParser; // Importa a classe CSVParser do pacote org.apache.commons.csv para fazer parsing do arquivo CSV.
+import org.apache.commons.csv.CSVRecord; // Importa a classe CSVRecord do pacote org.apache.commons.csv para representar um registro do CSV.
+import org.jfree.data.category.DefaultCategoryDataset; // Importa a classe DefaultCategoryDataset do pacote org.jfree.data.category para criar o conjunto de dados.
 
+/**
+ * Esta classe, ManipulacaoCSV, é responsável por ler um arquivo CSV, processar seus dados
+ * e criar um conjunto de dados para uso posterior.
+ */
 public class ManipulacaoCSV {
     
+    // Atributos para armazenar o ano selecionado, a coluna de entrada e a coluna de saída
     protected String anoSelecionado;
     protected String entradaColumn;
     protected String saidaColumn;
 
+    /**
+     * Construtor da classe ManipulacaoCSV.
+     * @param anoSelecionado O ano selecionado para filtrar os dados.
+     * @param entradaColumn A coluna que contém os valores de entrada.
+     * @param saidaColumn A coluna que contém os valores de saída.
+     */
     public ManipulacaoCSV(String anoSelecionado, String entradaColumn, String saidaColumn){
         this.anoSelecionado = anoSelecionado;
         this.entradaColumn = entradaColumn;
         this.saidaColumn = saidaColumn;
     }
 
+    /**
+     * Este método cria um conjunto de dados a partir de um arquivo CSV.
+     * @return Um DefaultCategoryDataset contendo os dados processados.
+     */
     protected DefaultCategoryDataset createDataset() {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
         try {
+            // Parse do arquivo CSV
             CSVParser parser = CSVParser.parse(
                     Paths.get("dados.csv").toFile(),
                     java.nio.charset.StandardCharsets.UTF_8,
@@ -49,25 +65,26 @@ public class ManipulacaoCSV {
                 }
             });
 
+            // TreeMap para armazenar saldos por ano
             TreeMap<String, Integer> saldosPorAno = new TreeMap<>(); 
             int saldoAnterior = 0;
 
+            // Processa os registros para calcular os saldos por ano
             for (CSVRecord record : records) {
                 String ano = record.get("Ano");
                 int entrada = Integer.parseInt(record.get(entradaColumn));
                 int saida = Integer.parseInt(record.get(saidaColumn));
 
                 saldoAnterior = saldoAnterior + entrada - saida;
-                System.out.println(saldoAnterior);
 
                 // Atualize o saldo no TreeMap com base no ano
                 saldosPorAno.put(ano, saldoAnterior);
             }
-
-            System.out.println(saldosPorAno);
+            
             int contador = 0;
             int saldo = 0;
 
+            // Processa os registros novamente para criar o conjunto de dados final
             for (CSVRecord record : records) {
                 String ano = record.get("Ano");
                 String mes = record.get("Mes");
@@ -99,5 +116,4 @@ public class ManipulacaoCSV {
 
         return dataset;
     }
-
 }
